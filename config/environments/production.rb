5f9709b3b0d392550c008ef411a8513e9011ee52
Brandon Ferguson <brfergus2@gmail.com>
2017-06-24 11:52:14 -0400

    heroku cables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://bf-chat-app.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://bf-chat-app.herokuapp.com', 'http://bf-chat-app.herokuapp.com']
 end
